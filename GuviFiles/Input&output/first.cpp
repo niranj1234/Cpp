1f0ac2b6f88a8488e986f0e9837e6cb481e324bf
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2025-04-01 11:09:25 -0700

    Update and rename first to first.cpp

---
+++ GuviFiles/Input&output/first.cpp
@@ -0,0 +1,25 @@
+// Write a code to get the input in the given format and print the output in the given format
+
+
+// Input Description:
+// To take an integer value
+
+// Output Description:
+// Print the integer value
+
+// Sample Input :
+// 2
+// Sample Output :
+// 2
+
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+   int name;
+    cin>>name;
+    
+    cout << name << endl;
+
+    return 0;
+}
