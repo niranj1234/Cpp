48bc26237ec48f339eb8aa9b7e2825da1afb7751
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2025-04-06 19:48:09 -0700

    Create 8.cpp

---
+++ GuviFiles/Input&output/8.cpp
@@ -0,0 +1,28 @@
+// Write a code to get the input in the given format and print the output in the given format.
+
+
+// Input Description:
+// A single line contains three float values separated by space.
+
+// Output Description:
+// Print the float value separated by line.
+
+// Sample Input :
+// 2.3 4.5 7.8
+// Sample Output :
+// 2.3
+// 4.5
+// 7.8
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a1, a2, a3;
+
+    cin >> a1 >> a2 >> a3;
+
+    cout << a1 << "\n" << a2 << "\n"<< a3 << endl;
+    return 0;
+}
