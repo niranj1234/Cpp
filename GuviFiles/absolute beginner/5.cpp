69e87e4bf29954821d314db0cd7d378b18b193bb
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2025-04-18 17:18:42 -0700

    Create 5.cpp

---
+++ GuviFiles/absolute beginner/5.cpp
@@ -0,0 +1,29 @@
+// You are provided with two numbers. Find and print the smaller number.
+
+
+// Input Description:
+// You are provided with two numbers as input.
+
+// Output Description:
+// Print the small number out of the two numbers.
+
+// Sample Input :
+// 23 1
+// Sample Output :
+// 1
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+int main() {
+    
+    // Write C++ code here
+    int n, m;
+    cin>> n >> m;
+    
+    if ( n < m) {
+        cout << n << endl;
+    } else {
+        cout << m;
+}
+}
