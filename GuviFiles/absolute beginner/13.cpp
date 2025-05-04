5561298cc8b10880190c317ff859814dfb89e076
niranj1234 <46585465+niranj1234@users.noreply.github.com>
2025-05-04 17:27:34 -0400

    Create 13.cpp

---
+++ GuviFiles/absolute beginner/13.cpp
@@ -0,0 +1,29 @@
+// You are provided with the radius of a circle "A". Find the length of its circumference.
+
+// Note: In case the output is coming in decimal, roundoff to 2nd decimal place. In case the input is a negative number, print "Error".
+
+
+// Input Description:
+// The Radius of a circle is provided as the input of the program.
+
+// Output Description:
+// Calculate and print the Circumference of the circle corresponding to the input radius up to two decimal places.
+
+// Sample Input :
+// 2
+// Sample Output :
+// 12.57
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+int main() {
+    int a;
+    double fact;
+    cin >> a;
+fact = 2*M_PI*a; 
+        cout  << fixed << setprecision(2) << fact <<endl;  
+    return 0;
+}
+
