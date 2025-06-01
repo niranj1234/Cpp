fec1f40a97fffb4c5363609bb5a179f8be0a25a3
Niranjan <46585465+niranj1234@users.noreply.github.com>
2025-06-01 10:34:30 -0400

    Update b22.cpp

--- GuviFiles/basics/b22.cpp
+++ GuviFiles/basics/b22.cpp
@@ -36,11 +36,8 @@ int main() {
     // Call the 'check' function with K (the target) and 'numb' (the vector).
     // Print the returned result (1 for found, 0 for not found) followed by a newline.
     int o =check(K, numb);
-    if(o==1){
-        cout<<"yes"<<endl;
-    }
-    else{
-        cout<<"no"<<endl;
-    }
+    if(o==1) cout<<"yes"<<endl;
+    
+    else cout<<"no"<<endl;
     return 0; // Indicate successful program execution
 }
