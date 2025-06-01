8f6a157d7b14e6869c2305ee7af42ecf187f154f
Niranjan <46585465+niranj1234@users.noreply.github.com>
2025-06-01 14:35:29 -0400

    Create b32.cpp

---
+++ GuviFiles/basics/b32.cpp
@@ -0,0 +1,27 @@
+// Given a string S consisting of 2 words reverse the order of two words .
+// Input Size : |S| <= 10000000
+// Sample Testcase :
+// INPUT
+// hello world
+// OUTPUT
+// world hello
+
+#include <string> 
+#include <iostream>
+using namespace std;
+int main() {
+
+    string S; 
+    
+    getline(cin, S);
+    
+    size_t space_pos = S.find(' ');
+
+    string word2 = S.substr(space_pos + 1);
+
+    string word1 = S.substr(0, space_pos);
+
+    std::cout << word2 << " " << word1 << std::endl;
+
+    return 0; 
+}
