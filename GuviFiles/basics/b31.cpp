1392ee72b4571f2d7d86ca60c06f0042005fc379
Niranjan <46585465+niranj1234@users.noreply.github.com>
2025-06-01 14:30:35 -0400

    Create b31.cpp

---
+++ GuviFiles/basics/b31.cpp
@@ -0,0 +1,18 @@
+// Given a number N, print the Bitwise NOT of that number.
+// Input Size : 1 <= N <= 10000
+// Sample Testcase :
+// INPUT
+// 5
+// OUTPUT
+// -6
+  
+#include <iostream>
+using namespace std;
+int main() {
+
+    int a;
+    cin>>a;
+    cout << ~a<<endl;
+
+    return 0;
+}
