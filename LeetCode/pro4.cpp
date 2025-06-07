d7f54f87f68d44f99226bd53fc56febec62fb220
Niranjan <46585465+niranj1234@users.noreply.github.com>
2025-06-06 22:25:48 -0400

    Update pro4.cpp

--- LeetCode/pro4.cpp
+++ LeetCode/pro4.cpp
@@ -1,14 +1,51 @@
 // https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        a=nums1+nums2
-        a.sort()
-        z = len(a)
-        if (len(a)%2 == 0):
-            d = len(a)//2
-            s= sum(a[d-1:d+1])/2
-        else:
-            x = len(a)//2
-            s= float(a[x])
-        return s
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        if (m > n) {
+            swap(nums1, nums2);
+            swap(m, n);
+        }
+
+        int imin = 0, imax = m, halfLen = (m + n + 1) / 2;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = halfLen - i;
+
+            if (i < m && nums2[j - 1] > nums1[i]) {
+                imin = i + 1; // i is too small
+            } else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                imax = i - 1; // i is too big
+            } else {
+                // i is perfect
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = max(nums1[i - 1], nums2[j - 1]);
+                }
+
+                if ((m + n) % 2 == 1) {
+                    return maxLeft; // Odd length
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = min(nums1[i], nums2[j]);
+                }
+
+                return (maxLeft + minRight) / 2.0; // Even length
+            }
+        }
+        return 0.0; // Should not reach here
+    }
+};
