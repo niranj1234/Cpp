0c1a900d6a619f8e7fc386b64fc81e6128545792
Niranjan <46585465+niranj1234@users.noreply.github.com>
2025-08-10 15:32:19 -0400

    Update 1.cpp

--- GuviFiles/array/1.cpp
+++ GuviFiles/array/1.cpp
@@ -41,3 +41,17 @@ int main() {
     
     return 0;
 }
+
+
+
+/*Reading input → O(n)
+Finding max and min indices in a single pass → O(n)
+Printing result → O(1)
+Total time complexity: O(n) + O(n) + O(1) → O(n)
+
+
+Space Complexity
+arr vector stores n integers → O(n) space.
+maxIndex, minIndex are integers → O(1).
+No extra large data structures.
+Total space complexity: O(n) for storing the array*/
