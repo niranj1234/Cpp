8a1d148068b28ce357a8cf3a2c9cf68af84f1e2d
Niranjan <46585465+niranj1234@users.noreply.github.com>
2025-08-11 15:41:33 -0400

    Update a8.cpp

--- GuviFiles/array/a8.cpp
+++ GuviFiles/array/a8.cpp
@@ -1,20 +1,61 @@
-#include<iostream>
+// You are a passport issuer, but due to some problems in the system, there are redundant  passport numbers. Your task is to delete all the duplicate passport numbers. You are given a list of passport numbers.
+
+
+// Input Description:
+// You are given length of list.Second line,You are given with a list.
+
+// Output Description:
+// Print the list of passport numbers without duplicates.
+
+// Sample Input :
+// 5
+// A23 B56 B56 C79 D16
+// Sample Output :
+// A23 B56 C79 D16
+
+#include <iostream>
 #include<vector>
+#include<algorithm>
+#include <string>
 
 
 using namespace std;
+int main() {
 
-int main(){
-    int n;
-    cin>>n;
-    vector<int> arr(n);
-    for(int i = 0; i < n; i++){
-        cin>>arr[i];
-    }
-
-for (int i = 0; i < n; i++){
-
-    cout<<arr[i]<<" ";
+   int c;
+   cin>>c;
+   
+if (c <= 0) {
+   return 0;
 }
-
+   vector<string> arr;
+   arr.reserve(c);
+       for (int i = 0; i < c; i++) {
+        string b; 
+        cin >> b; 
+        arr.push_back(b); 
+    }
+   
+//   sort(arr.begin(), arr.end());
+   
+  // arr.erase(unique(arr.begin(), arr.end()), arr.end());
+   
+   
+   for(int n = 0; n < arr.size(); ++n){
+        for(int m = n+1; m<arr.size();++m){
+            if(arr[n] == arr[m]){
+                arr.erase(arr.begin()+m);
+            }
+        }       
+   }
+   
+       for (size_t i = 0; i < arr.size(); ++i) {
+        cout << arr[i];
+        
+        if (i < arr.size() - 1) {
+            cout << " ";
+        }
+    }
+   cout << endl;
+    return 0;
 }
